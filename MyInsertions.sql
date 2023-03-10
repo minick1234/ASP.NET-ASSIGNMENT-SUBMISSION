b75a1b42b6d067f834bdfcb519707ea33f8b20c9
Dominik Ziolkowski <minickisbeast@gmail.com>
2023-03-09 19:11:41 -0500

    Added the sql

---
+++ MyInsertions.sql
@@ -0,0 +1,38 @@
+INSERT INTO Routes(routeLength, routeName, routeNumber, routePayPerKM)
+VALUES ('555', 'Extreme Route', '11', '12.50');
+
+INSERT INTO Routes(routeLength, routeName, routeNumber, routePayPerKM)
+VALUES ('95', 'Short Route', '12', '25.50');
+
+INSERT INTO Routes(routeLength, routeName, routeNumber, routePayPerKM)
+VALUES ('955', 'Deadly Route', '13', '11.50');
+
+INSERT INTO Routes(routeLength, routeName, routeNumber, routePayPerKM)
+VALUES ('123', 'Roundabout Route', '14', '19.50');
+
+INSERT INTO Routes(routeLength, routeName, routeNumber, routePayPerKM)
+VALUES ('325', 'Dongle Route', '15', '15.50');
+
+
+INSERT INTO Trucks(truckNum, truckModel, truckMake, truckRouteNumber)
+VALUES ('357', 'Toyota', 'BigMighty', '11');
+
+INSERT INTO Trucks(truckNum, truckModel, truckMake, truckRouteNumber)
+VALUES ('567', 'Ram', 'SlowLou', '15');
+
+INSERT INTO Trucks(truckNum, truckModel, truckMake, truckRouteNumber)
+VALUES ('390', 'Ford', 'Jellybean', '12');
+
+INSERT INTO Trucks(truckNum, truckModel, truckMake, truckRouteNumber)
+VALUES ('234', 'Chevy', 'HydroMydro', '12');
+
+INSERT INTO Trucks(truckNum, truckModel, truckMake, truckRouteNumber)
+VALUES ('531', 'Ford', 'Lug', '13');
+
+
+INSERT INTO Users(_FirstName, _LastName, _EmailAddress, _PhoneNumber,_Password, _ConfirmationPassword)
+VALUES ('Dominik', 'Ziolkowski', 'Dominik_2000@live.com', '416-819-4055', '123', '123');
+
+INSERT INTO Users(_FirstName, _LastName, _EmailAddress, _PhoneNumber,_Password, _ConfirmationPassword)
+VALUES ('Aneta', 'Ziolkowski', 'anetka197602@yahoo.com', '647-518-2951', '123', '123');
+
